6dd310de00b9949bbd3d0597f7a42523682300ce
Roy Sprowl <roys@jetstreamsoftware.com>
2017-12-11 17:44:17 -0800

    sha fix

--- jenkins/esp32_c.sh
+++ jenkins/esp32_c.sh
@@ -5,7 +5,7 @@ export PATH=$PATH:$ESP32_TOOLS/xtensa-esp32-elf/bin
 # Verify that the ESP32 SDK is at a tested commit
 pushd $IDF_PATH
 idf_commit=$(git rev-parse HEAD)
-tested_idf_commit=2c95a77cf93781f296883d5dbafcdc18e4389656
+tested_idf_commit=a7a861962daacec17bdb0f34fd59f16651b50b0f
 popd
 if [ $idf_commit = $tested_idf_commit ]
     then echo "ESP32 SDK commit is okay"
